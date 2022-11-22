8a4207a4dbfc8fad9b9e0c44e844b3892e47e70b
Sergii Karpus <karpus.sergey@gmail.com>
2022-11-23 00:30:17 +0200

    Update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -103,12 +103,6 @@ class Timer {
    }
 }
 
-// const timer = new Timer({
-//    onTick: updateClockFace,
-//    startTime: fp.selectedDates[0].getTime(),
-// });
-   
-
 
 // refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
 
@@ -118,8 +112,3 @@ function updateClockFace({ days, hours, minutes, seconds }) {
    refs.spanMinutes.textContent = `${minutes}`;
    refs.spanSeconds.textContent = `${seconds}`;
 };
-
-// Notiflix.Notify.success('Sol lucet omnibus');
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-// Notiflix.Notify.warning('Memento te hominem esse');
-// Notiflix.Notify.info('Cogito ergo sum');
