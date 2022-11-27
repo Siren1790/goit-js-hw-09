c2d38b9441e0a49a10afba6eb2d1eb9e13741dd8
Sergii Karpus <karpus.sergey@gmail.com>
2022-11-27 16:56:15 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
 }
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  let delay = e.target.delay.valueAsNumber;
+  let step = e.target.step.valueAsNumber;
+  let amount = e.target.amount.valueAsNumber;
+  
+  for (let i = 1; i <= amount; i++){
+    createPromise(i, delay)
+      .then(value => {
+        Notiflix.Notify.success(value);
+      })
+      .catch(value => {
+        Notiflix.Notify.failure(value);
+      })
+    delay += step;
+
+  }
+});
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }      
+    }, delay);
+  });
+
+  return promise;
+};
